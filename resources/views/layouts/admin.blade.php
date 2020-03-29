cf3b6fd5b17f9e74a542f2ed34bf14fd4fbdd8e8
manojbhatta <jmsbhatta@gmail.com>
2020-03-29 11:35:10 +0545

    updated admin panel UI

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -10,161 +10,41 @@
     <link href="{{ asset('assets/mdb/css/mdb.min.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/mdb/css/style.css') }}" rel="stylesheet">
     <link rel="icon" href="{{ asset('assets/img/logo.png') }}">
-    <style>
-        .custom-table thead {
-            background-color: #e8e8e8;
-            background-color: #eee;
-        }
-        
-        .custom-table thead th {
-            color: #656565;
-            color: #6c757d;
-            text-transform: uppercase;
-            font-size: 0.8em;
-        }
-        
-    </style>
-    <style>
-        body {
-            font-family: 'Nunito', sans-serif;
-        }
-        
-        .card-shadow {
-            box-shadow: 0 0px 15px 2px rgba(143, 143, 143, 0.09);
-        }
-        
-        .bg-ink {
-            background-color: #667bf1;
-        }
-        
-        .text-ink {
-            color: #667bf1;
-        }
-        
-        .bg-green {
-            background-color: #24e873;
-        }
-        
-        .text-green {
-            color: #24e873;
-        }
-        
-        .bg-red {
-            background-color: #fc5d51;
-        }
-        
-        .text-red {
-            color: #fc5d51;
-        }
-        
-        .side {
-            flex: 15%;
-            background-color: #384951;
-            height: 100vh;
-            position: sticky;
-            top: 0;
-            align-self: flex-start
-        }
-        
-        @media only screen and (max-width: 800px) {
-            .side {
-                background-color: #fff;
-                display: none;
-                width: 100%;
-                flex: 100%;
-                position: absolute: top: 0;
-                right: 0;
-            }
-            
-            .main {
-                flex: 100%;
-            }
-        }
-        
-        .main {
-            flex: 85%;
-            background-color: #f9f9f9;
-        }
-        
-        .sidebar-list {
-            font-weight: 400;
-        }
-        
-        .sidebar-list-item {
-            /*margin-top: 10px;*/
-            margin-bottom: px;
-        }
-        
-        .sidebar-list-item a {
-            color: #cbc9c9;
-        }
-        
-        .sidebar-list-item a:hover {
-            color: #fff;
-            color: #25c3ca;
-        }
-        
-        .sidebar-list-item a i {
-            margin-right: 15px;
-        }
-        
-        .sidebar-list-item a span {
-            float: right;
-        }
-        
-        .topbar-nav {
-            color: #667bf1;
-        }
-        
-        .topbar-nav i {
-            color: #667bf1;
-        }
-        
-        #dashboard {
-            color: #929394;
-        }
-        
-        h5.page-title {
-            font-weight: 500;
-            color: #598aa3;
-            color: #929394;
-        }
-        
-        .pagination .page-item.active .page-link {
-            box-shadow: none;
-        }
-        
-    </style>
-    
+    <link rel="stylesheet" href="{{ asset('assets/css/admin.css') }}">    
     @stack('styles')
     
 </head>
 <body>
     <div class="d-flex">
-        <div class="side pl-4">
+        <div class="side">
             <div class="py-4">
-                <h2 class="text-uppercase text-center text-light">
+                {{-- <h2 class="text-uppercase text-center text-light">
                     <a href="{{ url('/') }}" target="_blank">
-                        <img src="{{ asset('assets/img/logo-white.png') }}" alt="{{ __('Chhitomitho') }}" style="height: 60px;">
                     </a>
-                </h2>
+                </h2> --}}
+                <div class="user-details">
+                    <img class="rounded-circle mr-3" src="{{ Auth::user()->gravatar}}" alt="{{ __('Chhitomitho') }}" style="height: 80px; width: 80px; border-radius: 50%;">
+                    <div class="name">{{ Auth::user()->name }}</div>
+                    <div class="email">{{ Auth::user()->email }}</div>
+                    <div class="role">( {{ Auth::user()->role }} )</div>
+                </div>
             </div>
-            <div class="text-light text-uppercase small font-weight-bolder">Navigation</div>
+            <div class="text-muted text-uppercase small font-weight-bolder ml-2">Navigation</div>
             <ul class="list-unstyled sidebar-list">
                 <li class="sidebar-list-item">
-                    <a class="nav-link" href=""><i class="fas fa-tachometer-alt"></i>Dashboard <span><i class="fas fa-angle-right"></i></span></a>
+                    <a class="nav-link" href=""><i class="fas fa-desktop text-ink"></i>Dashboard <span><i class="fas fa-angle-right"></i></span></a>
                 </li>
                 <li class="sidebar-list-item">
-                    <a class="nav-link" href="{{ route('category.index') }}"><i class="far fa-list-alt"></i>Categories<span><i class="fas fa-angle-right"></i></span></a>
+                    <a class="nav-link" href="{{ route('category.index') }}"><i class="far fa-list-alt text-warning"></i>Categories<span><i class="fas fa-angle-right"></i></span></a>
                 </li>
                 <li class="sidebar-list-item">
-                    <a class="nav-link" href="{{ route('product.create') }}"><i class="fa fa-plus"></i>New Product<span><i class="fas fa-angle-right"></i></span></a>
+                    <a class="nav-link" href="{{ route('product.create') }}"><i class="fa fa-plus text-info"></i>New Product<span><i class="fas fa-angle-right"></i></span></a>
                 </li>
                 <li class="sidebar-list-item">
-                    <a class="nav-link" href="{{ route('product.index') }}"><i class="fas fa-cube"></i>Products<span><i class="fas fa-angle-right"></i></span></a>
+                    <a class="nav-link" href="{{ route('product.index') }}"><i class="fas fa-cube text-primary"></i>Products<span><i class="fas fa-angle-right"></i></span></a>
                 </li>
                 <li class="sidebar-list-item">
-                    <a class="nav-link" href="{{ route('users.index') }}"><i class="far fa-user"></i>Users<span><i class="fas fa-angle-right"></i></span></a>
+                    <a class="nav-link" href="{{ route('users.index') }}"><i class="far fa-user text-secondary"></i>Users<span><i class="fas fa-angle-right"></i></span></a>
                 </li>
                 <li class="sidebar-list-item">
                     <a class="nav-link" href=""><i class="fas fa-grip-horizontal"></i>Forum<span><i class="fas fa-angle-right"></i></span></a>
@@ -172,10 +52,51 @@
                 <li class="sidebar-list-item">
                     <a class="nav-link" href=""><i class="far fa-star"></i>Features<span><i class="fas fa-angle-right"></i></span></a>
                 </li>
+                <li class="sidebar-list-item">
+                    <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                        <i class="fas fa-sign-out-alt mr-2"></i>{{ __('Logout') }}
+                    </a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </li>
             </ul>
         </div>
         <div class="main">
-            <div class="card rounded-0" style="box-shadow: 0 2px 2px 0 rgba(143, 143, 143, 0.05);">
+            <div class="navbar navbar-expand-lg navbar-dark primary-color d-none">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item">
+                        <a class="nav-link" href=""><i class="fa fa-cog"></i></a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href=""><i class="fa fa-bell"></i></a>
+                    </li>
+                    
+                    <li class="nav-item">
+                        <a class="nav-link dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                            <i class="fas fa-sign-out-alt mr-2"></i>{{ __('Logout') }}
+                        </a>
+                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                            @csrf
+                        </form>
+                    </li>
+                    <li class="nav-item d-flex">
+                        <img class="rounded-circle mr-3" src="{{ Auth::user()->gravatar}}" alt="" style="height: 40px; width: 40px; border-radius: 50%;">
+                        <div class="d-flex flex-column">
+                            <div class="">{{ Auth::user()->name }}</div>
+                            <div class="text-capitalize">{{ Auth::user()->role }}</div>
+                        </div>
+                    </li>
+                </ul>
+                <ul class="navbar-nav ml-auto nav-flex-icons">
+                    <li class="nav-item avatar">
+                        <a class="nav-linkl" href="#">
+                            <img class="rounded-circle mr-3" src="{{ Auth::user()->gravatar}}" alt="" style="height: 40px; width: 40px; border-radius: 50%;">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <div class="card bg-primary rounded-0 d-none" style="box-shadow: 0 2px 2px 0 rgba(143, 143, 143, 0.05);">
                 <div class="d-flex px-4">
                     <ul class="nav topbar-nav ml-auto ">
                         <li class="nav-item align-self-center">
@@ -184,7 +105,7 @@
                         <li class="nav-item align-self-center">
                             <a class="nav-link grey-text" href=""><i class="fa fa-bell"></i></a>
                         </li>
-                        <li class="nav-item d-flex py-2 px-3 align-self-center" style="background-color: #f8f6ff;">
+                        <li class="nav-item d-flex py-2 px-3 align-self-center">
                             <img class="rounded-circle mr-3" src="{{ Auth::user()->gravatar}}" alt="" style="height: 40px; width: 40px; border-radius: 50%;">
                             <div class="d-flex flex-column">
                                 <div>{{ Auth::user()->name }}</div>
@@ -195,7 +116,6 @@
                             <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
                                 <i class="fas fa-sign-out-alt mr-2"></i>{{ __('Logout') }}
                             </a>
-                            
                             <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                 @csrf
                             </form>
