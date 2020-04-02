9b93c3049a99198ff8e38a26051d1b4a20b085ef
manojbhatta <jmsbhatta@gmail.com>
2020-04-02 14:13:02 +0545

    added dashboard link in navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -104,6 +104,7 @@
 								</a>
 
 								<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+									<a class="dropdown-item" href="{{ route(Auth::user()->role) }}">{{ __('Dashboard') }}</a>
 									<a class="dropdown-item" href="">{{ __('My Orders') }}</a>
 									<a class="dropdown-item" href="{{ route('user.profile') }}">{{ __('My Profile') }}</a>
 									<a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
