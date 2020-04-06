2dd150c1f8a4fbc19bb5177546d4f7db32eb3080
manojbhatta <jmsbhatta@gmail.com>
2020-04-07 00:52:32 +0545

    updated site title

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>{{ config('app.name', 'Chhitomitho') }}</title>
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
     <link href="{{ asset('assets/mdb/css/bootstrap.min.css') }}" rel="stylesheet">
     <link href="{{ asset('assets/mdb/css/mdb.min.css') }}" rel="stylesheet">
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,7 +5,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<!-- CSRF Token -->
 	<meta name="csrf-token" content="{{ csrf_token() }}">
-	<title>{{ config('app.name', 'Laravel') }}</title>
+	<title>{{ config('app.name', 'Chhitomitho') }}</title>
 	<link rel="icon" href="{{ asset('assets/img/logo.png') }}">
 	<!-- Fonts -->
 	<link rel="dns-prefetch" href="//fonts.gstatic.com">
