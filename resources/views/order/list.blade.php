e2a73f690f5b84fc7473fadcc9b1ca04d371fa61
manojbhatta <jmsbhatta@gmail.com>
2020-04-09 11:18:12 +0545

    removed unwanted code

--- resources/views/order/list.blade.php
+++ resources/views/order/list.blade.php
@@ -44,7 +44,7 @@
                         </tbody>
                     </table>
                 </div>
-
+                
                 @if($orders->hasPages())
                 <div class="d-flex">
                     <div class="ml-auto">
@@ -52,43 +52,8 @@
                     </div>
                 </div>
                 @endif
-
+                
             </div>
-            @foreach ($orders as $order)
-            <div class="card border rounded-0 mb-4 d-none">
-                <div class="card-header white">
-                    <div class="d-flex">
-                        <div>
-                            <h6>Order #{{ $order->id }}</h6>
-                            <div class="small text-muted">Placed on {{ \Carbon\Carbon::parse($order->created_at)->format('jS \\of F Y h:i:s A') }}</div>
-                        </div>
-                        <div class="ml-auto">
-                            <h6 class="h6-responsive text-capitalize">{{ $order->status }}</h6>
-                            <div class="small text-muted text-uppercase">{{ $order->payment_option }}</div>
-                        </div>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <table class="table table-bordered">
-                        @foreach ($order->orderProducts as $product)
-                        <tr>
-                            <td>{{ $product->name }}</td>
-                            <td><span class="text-muted">Qty:</span> {{ $product->quantity }}</td>
-                            <td><span class="text-muted">Delivered on</span> {{ \Carbon\Carbon::parse($order->created_at)->format('jS \\of F Y h:i A') }}</td>
-                        </tr>
-                        @endforeach
-                    </table>
-                    
-                    @if (!is_null($order->order_notes))
-                    <p class="card-text">
-                        <i class="mdb-color-text">Order Notes:</i>
-                        <br>
-                        {{ $order->order_notes }}
-                    </p>
-                    @endif
-                </div>
-            </div>
-            @endforeach
         </div>
     </div>
 </div>
