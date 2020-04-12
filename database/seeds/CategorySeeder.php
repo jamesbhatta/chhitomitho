be4b1cf7eff7e8ab0e87ef02bfe8ccf698872780
manojbhatta <jmsbhatta@gmail.com>
2020-04-12 14:43:09 +0545

    seeder

--- database/seeds/CategorySeeder.php
+++ database/seeds/CategorySeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class CategorySeeder extends Seeder
 {
@@ -11,6 +12,9 @@ class CategorySeeder extends Seeder
      */
     public function run()
     {
-        //
+        DB::table('categories')->insert([
+            'name' => 'Uncategorized',
+            'slug' => 'uncategorized'
+        ]);
     }
 }
