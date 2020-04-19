1ff045b5c413b05b8a7e16bfa89d91ef0bf3a43e
manojbhatta <jmsbhatta@gmail.com>
2020-04-19 14:03:17 +0545

    configuration for sms to owner and courier

--- app/Listeners/OrderStatusChangedListener.php
+++ app/Listeners/OrderStatusChangedListener.php
@@ -33,46 +33,50 @@ class OrderStatusChangedListener  implements ShouldQueue
     {
         \DB::connection()->enableQueryLog();
         $event->order->load('store', 'store.owner:id,mobile', 'courier:id,mobile');
-
+        
         // dump($event->order->store->owner);
         // $event->order->store->owner->id;
         // $event->order->courier_id;
         // $queries = \DB::getQueryLog();
         // return dd($queries);
-
+        
         switch ($event->order->status) {
             case "pending":
                 // Send Notification to Database and Slack
             break;
-
+            
             case "confirmed":
                 // Send sms to Customer
                 $to = $event->order->billing_phone;
                 $message = createOrderConfirmedSMS($event->order);
                 SendSmsJob::dispatch($to, $message);
-
+                
                 // Send SMS to Partner (Store owner)
-                $to = $event->order->store->owner->mobile;
-                $message = createConfirmedOrderSMSForPartner($event->order);
-                SendSmsJob::dispatch($to, $message);
+                if (config('notificaiton.sms.notify_store')) {
+                    $to = $event->order->store->owner->mobile;
+                    $message = createConfirmedOrderSMSForPartner($event->order);
+                    SendSmsJob::dispatch($to, $message);
+                }
                 
                 // Send SMS to courier
-                $to = $event->order->courier->mobile;
-                $message = createConfirmedOrderSMSForCourier($event->order, $event->order->store->name);
-                SendSmsJob::dispatch($to, $message);
+                if (config('notificaiton.sms.notify_courier')) {
+                    $to = $event->order->courier->mobile;
+                    $message = createConfirmedOrderSMSForCourier($event->order, $event->order->store->name);
+                    SendSmsJob::dispatch($to, $message);
+                }
                 
                 // Send Database Notification to Partner (store owner) and Courier
                 $receivers = \App\User::whereIn('id', [$event->order->store->owner->id, $event->order->courier_id])->get();
                 Notification::send($receivers, new OrderConfirmed($event->order));
             break;
-
+            
             case "shipped":
                 // Send SMS to customer
                 $to = $event->order->billing_phone;
                 $message = createOrderShippedSMS($event->order);
                 SendSmsJob::dispatch($to, $message);
             break;
-
+            
             case "delivered":
                 // Send SMS to customer and Notify in Slack
                 $to = $event->order->billing_phone;
--- config/notification.php
+++ config/notification.php
@@ -4,6 +4,8 @@ return [
         'token' => env('SMS_TOKEN'),
         'sender' => env('SMS_SENDER'),
         'url' => env('SMS_URL', 'http://sms.bmpinfology.com/api/v3/sms?'),
+        'notify_store' => env('SMS_STORE', false),
+        'notify_courier' => env('SMS_COURIER', false),
     ],
 
     'slack' => [
