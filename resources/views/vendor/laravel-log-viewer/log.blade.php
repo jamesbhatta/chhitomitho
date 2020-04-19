93dae39b3f97476e562763493be14340a17122f2
manojbhatta <jmsbhatta@gmail.com>
2020-04-19 21:39:23 +0545

    UI improvement

--- resources/views/vendor/laravel-log-viewer/log.blade.php
+++ resources/views/vendor/laravel-log-viewer/log.blade.php
@@ -98,109 +98,104 @@
               @if ($current_folder == $folder)
               <div class="list-group folder">
                 @foreach($folder_files as $file)
-                <a href="?l={{ \Illuminate\Support\Facades\Crypt::encrypt($file) }}&f={{ \Illuminate\Support\Facades\Crypt::encrypt($folder) }}"
-                class="list-group-item rounded-0 @if ($current_file == $file) llv-active @endif">
-                {{$file}}
-              </a>
-              @endforeach
+                <a href="?l={{ \Illuminate\Support\Facades\Crypt::encrypt($file) }}&f={{ \Illuminate\Support\Facades\Crypt::encrypt($folder) }}" class="list-group-item rounded-0 @if ($current_file == $file) llv-active @endif">
+                  {{$file}}
+                </a>
+                @endforeach
+              </div>
+              @endif
             </div>
-            @endif
-          </div>
+            @endforeach
+            @foreach($files as $file)
+            <a href="?l={{ \Illuminate\Support\Facades\Crypt::encrypt($file) }}"
+            class="list-group-item rounded-0 @if ($current_file == $file) llv-active @endif">
+            {{$file}}
+          </a>
           @endforeach
-          @foreach($files as $file)
-          <a href="?l={{ \Illuminate\Support\Facades\Crypt::encrypt($file) }}"
-          class="list-group-item rounded-0 @if ($current_file == $file) llv-active @endif">
-          {{$file}}
-        </a>
-        @endforeach
+        </div>
       </div>
     </div>
-  </div>
-  <div class="col-md-10 table-container bg-white py-3">
-    @if ($logs === null)
-    <div>
-      Log file >50M, please download it.
-    </div>
-    @else
-    <table id="table-log" class="table table-bordered table-hover table-responsive-sm dt-responsive" data-ordering-index="{{ $standardFormat ? 2 : 0 }}">
-      <thead class="thead-light">
-        <tr>
-          @if ($standardFormat)
-          <th>Level</th>
-          <th>Context</th>
-          <th>Date</th>
-          @else
-          <th>Line number</th>
-          @endif
-          <th>Content</th>
-        </tr>
-      </thead>
-      <tbody>
-        
-        @foreach($logs as $key => $log)
-        <tr data-display="stack{{{$key}}}">
-          @if ($standardFormat)
-          <td class="nowrap text-{{{$log['level_class']}}}">
-            <span class="fa fa-{{{$log['level_img']}}}" aria-hidden="true"></span>&nbsp;&nbsp;{{$log['level']}}
-          </td>
-          <td class="text">{{$log['context']}}</td>
-          @endif
-          <td class="date">{{{$log['date']}}}</td>
-          <td class="text">
+    <div class="col-md-10 table-container bg-white py-3">
+      @if ($logs === null)
+      <div>
+        Log file >50M, please download it.
+      </div>
+      @else
+      <table id="table-log" class="table table-bordered table-hover table-responsive-sm dt-responsive" data-ordering-index="{{ $standardFormat ? 2 : 0 }}">
+        <thead class="thead-light">
+          <tr>
+            @if ($standardFormat)
+            <th>Level</th>
+            <th>Context</th>
+            <th>Date</th>
+            @else
+            <th>Line number</th>
+            @endif
+            <th>Content</th>
+          </tr>
+        </thead>
+        <tbody>
+          
+          @foreach($logs as $key => $log)
+          <tr data-display="stack{{{$key}}}">
+            @if ($standardFormat)
+            <td class="nowrap text-{{{$log['level_class']}}}">
+              <span class="fa fa-{{{$log['level_img']}}}" aria-hidden="true"></span>&nbsp;&nbsp;{{$log['level']}}
+            </td>
+            <td class="text">{{$log['context']}}</td>
+            @endif
+            <td class="date">{{{$log['date']}}}</td>
+            <td class="text">
+              @if ($log['stack'])
+              <button type="button"
+              class="float-right expand btn btn-outline-dark btn-sm mb-2 ml-2"
+              data-display="stack{{{$key}}}">
+              <span class="fa fa-search"></span>
+            </button>
+            @endif
+            {{{$log['text']}}}
+            @if (isset($log['in_file']))
+            <br/>{{{$log['in_file']}}}
+            @endif
             @if ($log['stack'])
-            <button type="button"
-            class="float-right expand btn btn-outline-dark btn-sm mb-2 ml-2"
-            data-display="stack{{{$key}}}">
-            <span class="fa fa-search"></span>
-          </button>
-          @endif
-          {{{$log['text']}}}
-          @if (isset($log['in_file']))
-          <br/>{{{$log['in_file']}}}
-          @endif
-          @if ($log['stack'])
-          <div class="stack" id="stack{{{$key}}}"
-          style="display: none; white-space: pre-wrap;">{{{ trim($log['stack']) }}}
-        </div>
-        @endif
-      </td>
-    </tr>
-    @endforeach
-    
-  </tbody>
-</table>
-@endif
-<div class="p-3">
-  @if($current_file)
-  <a href="?dl={{ \Illuminate\Support\Facades\Crypt::encrypt($current_file) }}{{ ($current_folder) ? '&f=' . \Illuminate\Support\Facades\Crypt::encrypt($current_folder) : '' }}">
-    <span class="fa fa-download"></span> Download file
-  </a>
-  <span class="mx-3">|</span>
-  <a id="clean-log" href="?clean={{ \Illuminate\Support\Facades\Crypt::encrypt($current_file) }}{{ ($current_folder) ? '&f=' . \Illuminate\Support\Facades\Crypt::encrypt($current_folder) : '' }}">
-    <span class="fa fa-sync"></span> Clean file
-  </a>
-  <span class="mx-3">|</span>
-  <a id="delete-log" href="?del={{ \Illuminate\Support\Facades\Crypt::encrypt($current_file) }}{{ ($current_folder) ? '&f=' . \Illuminate\Support\Facades\Crypt::encrypt($current_folder) : '' }}">
-    <span class="fa fa-trash"></span> Delete file
-  </a>
-  @if(count($files) > 1)
-  -
-  <a id="delete-all-log" href="?delall=true{{ ($current_folder) ? '&f=' . \Illuminate\Support\Facades\Crypt::encrypt($current_folder) : '' }}">
-    <span class="fa fa-trash-alt"></span> Delete all files
-  </a>
-  @endif
-  @endif
-</div>
-</div>
+            <div class="stack" id="stack{{{$key}}}" style="display: none; white-space: pre-wrap;">
+              {{{ trim($log['stack']) }}}
+            </div>
+            @endif
+          </td>
+        </tr>
+        @endforeach
+        
+      </tbody>
+    </table>
+    @endif
+    <div class="p-3">
+      @if($current_file)
+      <a href="?dl={{ \Illuminate\Support\Facades\Crypt::encrypt($current_file) }}{{ ($current_folder) ? '&f=' . \Illuminate\Support\Facades\Crypt::encrypt($current_folder) : '' }}">
+        <span class="fa fa-cloud-download-alt"></span> Download file
+      </a>
+      <span class="mx-3">|</span>
+      <a id="clean-log" href="?clean={{ \Illuminate\Support\Facades\Crypt::encrypt($current_file) }}{{ ($current_folder) ? '&f=' . \Illuminate\Support\Facades\Crypt::encrypt($current_folder) : '' }}">
+        <span class="fa fa-sync"></span> Clean file
+      </a>
+      <span class="mx-3">|</span>
+      <a id="delete-log" href="?del={{ \Illuminate\Support\Facades\Crypt::encrypt($current_file) }}{{ ($current_folder) ? '&f=' . \Illuminate\Support\Facades\Crypt::encrypt($current_folder) : '' }}">
+        <span class="far fa-trash-alt"></span> Delete file
+      </a>
+      @if(count($files) > 1)
+      <span class="mx-3">|</span>
+      <a id="delete-all-log" href="?delall=true{{ ($current_folder) ? '&f=' . \Illuminate\Support\Facades\Crypt::encrypt($current_folder) : '' }}">
+        <span class="fas fa-trash-alt"></span> Delete all files
+      </a>
+      @endif
+      @endif
+    </div>
+  </div>
 </div>
 </div>
 <!-- jQuery for Bootstrap -->
-<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
-crossorigin="anonymous"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"
-integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-crossorigin="anonymous"></script>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 <!-- FontAwesome -->
 <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
 <!-- Datatables -->
