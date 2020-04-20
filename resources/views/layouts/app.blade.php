6573b47e29e475f1f3ad8846f6f173bd3f039427
manojbhatta <jmsbhatta@gmail.com>
2020-04-20 21:41:09 +0545

    dropdown styles

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,17 +9,17 @@
 	<link rel="icon" href="{{ asset('assets/img/logo.png') }}">
 	<!-- Fonts -->
 	<link rel="dns-prefetch" href="//fonts.gstatic.com">
-
+	
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css">
 	<link href="{{ asset('assets/mdb/css/bootstrap.min.css') }}" rel="stylesheet">
 	<link href="{{ asset('assets/mdb/css/mdb.min.css') }}" rel="stylesheet">
 	<link href="{{ asset('assets/mdb/css/style.css') }}" rel="stylesheet">
-
+	
 	@stack('styles')
 	<link href="https://fonts.googleapis.com/css?family=Lora&display=swap" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Permanent+Marker&display=swap" rel="stylesheet">
 	<link href="https://fonts.googleapis.com/css?family=Sen&display=swap" rel="stylesheet">
-
+	
 	<style>
 		html {
 			scroll-behavior: smooth;
@@ -59,8 +59,26 @@
 		.custom-navbar .nav-item .nav-link {
 			color: #f1f1f1!important;
 		}
+		.custom-navbar .dropdown-menu {
+			margin-top: 10px!important;
+			border-radius: 0;
+		}
+		.custom-navbar .dropdown-menu:before {
+			content: "";
+			border-bottom: 10px solid #fff;
+			border-right: 10px solid transparent;
+			border-left: 10px solid transparent;
+			position: absolute;
+			top: -10px;
+			right: 16px;
+			z-index: 10;
+		}
+		.custom-navbar .dropdown-menu a {
+			color: #535b61;
+			padding: .25rem 1.5rem;
+		}
 	</style>
-
+	
 	{{--
 		Theme color Brown: #982121
 		Secondary Color: #ffb80e
@@ -77,13 +95,13 @@
 					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
 						<span class="navbar-toggler-icon"></span>
 					</button>
-
+					
 					<div class="collapse navbar-collapse" id="navbarSupportedContent">
 						<!-- Left Side Of Navbar -->
 						<ul class="navbar-nav mr-auto">
-
+							
 						</ul>
-
+						
 						<!-- Right Side Of Navbar -->
 						<ul class="navbar-nav ml-auto">
 							<li class="nav-item">
@@ -108,18 +126,18 @@
 								<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
 									<img src="{{ Auth::user()->avatar }}" alt="" style="width: 40px; height:40px; border-radius: 50%;">
 									{{ Auth::user()->name }} <span class="caret"></span>
-								</a>	
-
-								<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+								</a>
+								<div class="dropdown-menu dropdown-menu-right card-shadow" aria-labelledby="navbarDropdown">
 									@can('access-backend')
 									<a class="dropdown-item" href="{{ route(Auth::user()->role) }}">{{ __('Dashboard') }}</a>
 									@endcan
 									<a class="dropdown-item" href="{{ route('customer.orders') }}">{{ __('My Orders') }}</a>
 									<a class="dropdown-item" href="{{ route('user.profile') }}">{{ __('My Profile') }}</a>
+									<div class="dropdown-divider"></div>
 									<a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
 										{{ __('Logout') }}
 									</a>
-
+									
 									<form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
 										@csrf
 									</form>
@@ -130,12 +148,12 @@
 					</div>
 				</div>
 			</nav>
-
+			
 			<main class="py-4">
 				@yield('content')
 			</main>
 		</div>
-
+		
 		{{-- Scripts --}}
 		<script type="text/javascript" src="{{ asset('assets/mdb/js/jquery.min.js') }}"></script>
 		<script type="text/javascript" src="{{ asset('assets/mdb/js/popper.min.js') }}"></script>
@@ -148,7 +166,7 @@
 						'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
 					}
 				});
-
+				
 				App = {
 					loadCartSummary: function() {
 						$.ajax({
@@ -172,8 +190,9 @@
 				App.loadCartSummary();
 			});
 		</script>
-
+		
 		@stack('scripts')
-
+		
 	</body>
 	</html>
+	
