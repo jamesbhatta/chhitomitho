403c9c5b22fa19acd44ece55c2402dbc9191a9ac
manojbhatta <jmsbhatta@gmail.com>
2020-04-20 21:55:52 +0545

    user icon in navbar

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -107,7 +107,7 @@
 							<li class="nav-item">
 								<a class="nav-link" href="{{ route('cart') }}">
 									<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" style="fill: #f1f1f1;" viewBox="0 0 24 24"><path d="M10 19.5c0 .829-.672 1.5-1.5 1.5s-1.5-.671-1.5-1.5c0-.828.672-1.5 1.5-1.5s1.5.672 1.5 1.5zm3.5-1.5c-.828 0-1.5.671-1.5 1.5s.672 1.5 1.5 1.5 1.5-.671 1.5-1.5c0-.828-.672-1.5-1.5-1.5zm1.336-5l1.977-7h-16.813l2.938 7h11.898zm4.969-10l-3.432 12h-12.597l.839 2h13.239l3.474-12h1.929l.743-2h-4.195z"/></svg>
-									<span id="cart-total-quantity" class="badge badge-pill badge-info mr-1"></span>
+									<span id="cart-total-quantity" class="badge badge-pill badge-info card-shadow mr-1"></span>
 									<span id="cart-total-price" class="small"></span>
 								</a>
 							</li>
@@ -124,7 +124,7 @@
 							@else
 							<li class="nav-item dropdown">
 								<a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-									<img src="{{ Auth::user()->avatar }}" alt="" style="width: 40px; height:40px; border-radius: 50%;">
+									<img class="d-inline" src="{{ Auth::user()->gravatar }}" alt="" style="width: 30px; height:30px; border-radius: 50%;">
 									{{ Auth::user()->name }} <span class="caret"></span>
 								</a>
 								<div class="dropdown-menu dropdown-menu-right card-shadow" aria-labelledby="navbarDropdown">
